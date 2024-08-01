1d40247fdaaa4a3815dd72797ed718639e16d03a
omegacc <alphabity>
2024-08-01 20:58:33 +0800

    fix bug

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("feed/rss", LatestPostsFeed(), name="post_feed"),
     path("feed/atom", AtomSiteNewsFeed()),
     path("", views.PostList.as_view(), name="home"),
+    path("blog/", views.PostList.as_view(), name="blog"),
     # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
 ]
